7fa932a6f42d885addbac725e29759909561de3e
Personal <detulande@unicauca.edu.co>
2020-02-19 08:09:58 -0500

    2do

--- src/unicauca/negocio/GestorUsuarioBD.java
+++ src/unicauca/negocio/GestorUsuarioBD.java
@@ -25,16 +25,26 @@ public class GestorUsuarioBD {
         conector.conectarse();
         conector.crearConsulta("select * from vigilante as us where us.vigusuario = '" + user + "' and us.vigcontra = '" + password + "'");
         Usuario miUsuario = null;
+        String user_user="";
+        String user_password="";
+        String user_privilegio="";
         if (conector.getResultado().next()) {
-            String user_cedula = conector.getResultado().getString("idcedula");
-            String user_nombres = conector.getResultado().getString("connombres");
-            String user_apellidos = conector.getResultado().getString("conapellidos");
-            String user_fechaNaci = conector.getResultado().getString("confechanacimiento");
-            String user_user = conector.getResultado().getString("vigusuario");
-            String user_password = conector.getResultado().getString("vigcontra");
-            String user_privilegio = "Vigilante";
-            miUsuario = new Usuario(user_cedula, user_nombres, user_apellidos, user_fechaNaci, user_user, user_password,user_privilegio);
+            user_user = conector.getResultado().getString("vigusuario");
+            user_password = conector.getResultado().getString("vigcontra");
+            user_privilegio = "Vigilante";
+        }else{
+            conector.crearConsulta("select * from administrador as us where us.usuaradmin = '" + user + "' and us.usuarcontras = '" + password + "'");
+            if (conector.getResultado().next()) {
+                user_user = conector.getResultado().getString("usuaradmin");
+                user_password = conector.getResultado().getString("usuarcontras");
+                user_privilegio="Administador";
+            }
         }
+        String user_cedula = conector.getResultado().getString("idcedula");
+        String user_nombres = conector.getResultado().getString("connombres");
+        String user_apellidos = conector.getResultado().getString("conapellidos");
+        String user_fechaNaci = conector.getResultado().getString("confechanacimiento");
+        miUsuario = new Usuario(user_cedula, user_nombres, user_apellidos, user_fechaNaci, user_user, user_password,user_privilegio);
         conector.desconectarse();
         return miUsuario;
     }
